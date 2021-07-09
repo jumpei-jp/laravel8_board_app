3bfa705614c9215a9c027664bc4974c911428a79
jumpei-jp <junpeiyamaoka0516@gmail.com>
2021-07-09 11:01:57 +0900

    生徒情報追加機能作成
    % php artisan vendor:publish --tag=jetstream-views
    ↑bladeのcomponentを使えるようにインストール
    そして、追加機能の作成を行った。
    
    「追加する」の文字が出てこない。

--- myapp/app/Http/Livewire/Students.php
+++ myapp/app/Http/Livewire/Students.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\Student; //追加分
 use Livewire\Component;
 
+
 class Students extends Component
 {
     public function render()
@@ -12,4 +13,46 @@ class Students extends Component
         $students = Student::orderBy('id', 'DESC')->get(); //studentのidを全件取得
         return view('livewire.students.index', compact('students'));
     }
+
+    public $ids;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $phone;
+    public $modalStatus;
+
+    public function openModal() //modalStatusをtrueにしmodalを表示する
+    {
+        $this->resetInputFields();
+        $this->modalStatus = true;
+    }
+
+    public function closeModal() //modalStatusをfalseにしmodalを閉じる
+    {
+        $this->modalStatus = false;
+    }
+
+    public function resetInputFields() //Inputタグの中身を空にする
+    {
+        $this->firstname = '';
+        $this->lastname = '';
+        $this->email = '';
+        $this->phone = '';
+    }
+
+    public function store() //バリデーションをした後に、生徒のデータを保存
+    {
+        $validatedData = $this->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+        ]);
+        Student::create($validatedData);
+        session()->flash('message', '新規投稿に成功しました。');
+        $this->resetInputFields();
+        $this->closeModal();
+    }
+
 }
+
